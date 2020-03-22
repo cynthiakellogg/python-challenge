5f6282ed0e3fcdb4e8792ac756fb26661f74971a
Cynthia <cynthiakellogg@gmail.com>
2020-03-21 18:46:20 -0700

    heres what I've got

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,15 +3,11 @@ import csv
 
 #link to the data
 pybank_csv = os.path.join("Resources", "PyBank_data.csv")
-
-
 #list to store the data
 profitList = []
 changeList = []
 averacgeChange = []
 dateList = []
-
-
 #read the data, but omit the headers in row 1
 with open(pybank_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
@@ -20,7 +16,7 @@ with open(pybank_csv) as csvfile:
     csv_header = next(csvfile)
     #print(f"Header: {csv_header} ")
     count = 0
-
+    date = 0
  # Read through each row of data after the header
     for row in csv_reader: 
         profitList.append(row[1])
@@ -28,21 +24,27 @@ with open(pybank_csv) as csvfile:
         count+= 1
         if count == 1:
            previous = float(row[1])
+           date = str(row[0])
         elif count > 1:
            change = float(row[1]) - previous
            changeList.append(change)
-           previous = float(row[1])        
+           previous = float(row[1]) 
+           date = str(row[0])
+                         
     
     total_change = sum(changeList)
     averageChange = (total_change/ (count - 1))
-    greatest_increase = max(changeList) 
+    greatest_increase = max(changeList)
+    #greatest_date = row[0, greatest_increase]
+    
+   
     greatest_decrease = min(changeList)
     #print it to a summary
     print(f"Summary Title: Financial Analysis")
     print(f"Total Months: {count}")  
     print(f"Average Change: {averageChange}")
-    print(f"Greatest Increase In Profits: {greatest_increase}")
-    print(f"Greatest Decrease In Profits: {greatest_decrease}") 
+    print(f"Greatest Increase In Profits: {date} {greatest_increase}")
+    print(f"Greatest Decrease In Profits: {date} {greatest_decrease}") 
     row1 = (f"Total Months: {count}")  
     row2 = (f"Average Change: {averageChange}")
     row3 = (f"Greatest Increase In Profits: {greatest_increase}")
